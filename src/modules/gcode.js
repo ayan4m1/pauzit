1c63ff573e0951e013c5149bb3f0d18025bf96d5
ayan4m1 <andrew@bulletlogic.com>
2022-04-15 19:49:01 -0400

    feat: implement tool change replacements

--- src/modules/gcode.js
+++ src/modules/gcode.js
@@ -8,9 +8,27 @@ import { getLogger } from './logging.js';
 
 const log = getLogger('gcode');
 
+const extruderChangePattern = /^T[0-9]+ ; change extruder$/gm;
+const toolChangePattern =
+  /^; CP TOOLCHANGE START$\n^; toolchange #([0-9]+)$\n^; material : (\w+) -> (\w+)$\n^;-+$\n^((.|\n)+)$\n^; CP TOOLCHANGE UNLOAD$/gm;
 const transformer = new Transform({
   transform: (chunk, _, callback) =>
-    callback(null, chunk.toString('utf8').toUpperCase())
+    callback(
+      null,
+      chunk
+        .toString('utf8')
+        .replace(extruderChangePattern, '')
+        .replace(
+          toolChangePattern,
+          (_, toolChange, oldMaterial, newMaterial, innerCode) => `
+; FILAMENT SWAP START
+; toolchange #${toolChange}
+; material : ${oldMaterial} -> ${newMaterial}
+${innerCode}
+M600
+; CP TOOLCHANGE UNLOAD`
+        )
+    )
 });
 
 export const processFile = async (input, output) => {
